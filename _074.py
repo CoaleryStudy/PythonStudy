7aa401f1805ccacd1cce5c7d05701e9bc08443b9
Coalery <doralife12@naver.com>
2020-04-27 23:34:02 +0900

    074

--- _074.py
+++ _074.py
@@ -1,7 +1,7 @@
-def html_tag(tag):
+def html_tag(htag):
     def real_decorator(func):
         def wrapper():
-            return '<{0}>{1}</{0}>'.format(tag, func())
+            return '<{0}>{1}</{0}>'.format(htag, func())
         return wrapper
     return real_decorator
 
